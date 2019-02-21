7a5b759e7f08cae05252450dc80d5ecc8719b2d2
Jesús Suárez Molina <jesuss@apolo.local>
2019-02-21 12:33:57 -0300

    Angular material integrated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { postReducer } from './post.reducer';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,11 +16,15 @@ import { postReducer } from './post.reducer';
   imports: [
     BrowserModule,
     FormsModule,
+    MaterialModule,
+    FlexLayoutModule,
 
     // Reducer declaration.
     StoreModule.forRoot({
       post: postReducer
-    })
+    }),
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
