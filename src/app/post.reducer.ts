63d5131928d940fd81f4e2034d8f2ef2784d7c9d
Jesús Suárez Molina <jesuss@apolo.local>
2019-07-25 17:14:27 -0300

    Update.

--- src/app/post.reducer.ts
+++ src/app/post.reducer.ts
@@ -4,28 +4,28 @@ import { Post } from './post';
 export type Action = PostActions.All;
 
 const initialState: Post = {
-    text: 'This is the initial title.',
-    likes: 0
+  text: 'This is the initial title.',
+  likes: 0
 };
 
 const newState = (state, newData) => {
-    return Object.assign({}, state, newData);
+  return { ...state, ...newData };
 };
 
 export function postReducer(state: Post = initialState, action: any) {
-    console.log(action.type, state);
+  console.log(action.type, state);
 
-    switch (<any>action.type) {
-        case PostActions.EDIT_TEXT:
-            return newState(state, { text: action.payload });
-        case PostActions.UPVOTE_TEXT:
-            return newState(state, { likes: state.likes + 1 });
-        case PostActions.DOWNVOTE_TEXT:
-            return newState(state, { likes: state.likes - 1 });
-        case PostActions.RESET_TEXT:
-            return initialState;
+  switch (<any>action.type) {
+    case PostActions.EDIT_TEXT:
+      return newState(state, { text: action.payload });
+    case PostActions.UPVOTE_TEXT:
+      return newState(state, { likes: state.likes + 1 });
+    case PostActions.DOWNVOTE_TEXT:
+      return newState(state, { likes: state.likes - 1 });
+    case PostActions.RESET_TEXT:
+      return initialState;
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
